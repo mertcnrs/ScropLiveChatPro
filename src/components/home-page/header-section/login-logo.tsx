f4ee02b91413f4f2bef6df68fbbbfc607a901af3
mertcnrs <mertcnr15@gmail.com>
2025-02-05 23:48:59 +0300

    Update: Logo anasayfaya yönlendirme linki eklendi

--- src/components/home-page/header-section/login-logo.tsx
+++ src/components/home-page/header-section/login-logo.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Box } from '@radix-ui/themes';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function LoginLogoComponent(): React.ReactNode {
   return (
     <Box>
       <div className="hidden lg:block">
-        <Image
-          src={'/ranchat-main.png'}
-          alt="ranchat-main"
-          width={'160'}
-          height={'160'}
-          className="object-contain"
-        />
+        <Link href="/">
+          <Image
+            src={'/ranchat-main.png'}
+            alt="ranchat-main"
+            width={'160'}
+            height={'160'}
+            className="object-contain cursor-pointer"
+          />
+        </Link>
       </div>
       <div className="block lg:hidden">
-        <Image
-          src={'/ranchat-logo.png'}
-          alt="ranchat-logo"
-          width={'40'}
-          height={'40'}
-          className="object-contain"
-        />
+        <Link href="/">
+          <Image
+            src={'/ranchat-logo.png'}
+            alt="ranchat-logo"
+            width={'40'}
+            height={'40'}
+            className="object-contain cursor-pointer"
+          />
+        </Link>
       </div>
       {/* <Button radius={'small'}>Login</Button> */}
       {/* <Button radius={'small'}>Register</Button> */}
